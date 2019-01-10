0eb2f57a3a52f3aa3e1d4e9b5b8537c50c2f7cad
Franklin Sobrinho <franklin.g.sobrinho@outlook.com>
2019-01-10 16:13:05 -0300

    Create abuild user on Dockerfile, instead of in docker-entrypoint.sh

--- build/abuild/docker-entrypoint.sh
+++ build/abuild/docker-entrypoint.sh
@@ -2,35 +2,12 @@
 
 set -e
 
-if [ -z $USERID ]; then
-    echo "Environment variable 'USERID' not set, add '-e USERID=\$(id -u)' to run options."
-    return 1
-fi
-
-add_builder_user() {
-    if id -u ${USERID} > /dev/null 2>&1; then
-        return
-    fi
-
-    adduser -h $ABUILD_HOME -H -u "$USERID" -D builder
-	addgroup builder abuild
-
-	echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-}
-
 if [ $(id -u) -eq 0 ]; then
-    add_builder_user
-
-    chown -R "builder" .
-
-    if [ -f "$PACKAGER_PRIVKEY" ]; then
-        chmod g+r $PACKAGER_PRIVKEY
-        chgrp -R abuild $PACKAGER_PRIVKEY
-    fi
+    chown -R abuild ./packages
 
     if [ "$1" = 'apk-build' ]; then
         apk update
-        exec su-exec builder "$0" "$@"
+        exec su-exec abuild "$0" "$@"
     fi
 
     if [ "$1" = 'abuild' ]; then
@@ -39,7 +16,7 @@ if [ $(id -u) -eq 0 ]; then
     fi
 
     if [ "$1" = 'abuild-keygen' ]; then
-        exec su-exec builder "$0" "$@"
+        exec su-exec abuild "$0" "$@"
     fi
 fi
 
